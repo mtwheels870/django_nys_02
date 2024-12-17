60ecfec66a9ec73c53f9a6e9686b351a9bd653cf
Michael Wheeler <michael.wheeler@compassblue.ai>
2024-12-17 15:03:30 -0500

    /polls/app_nys/

--- ny_viz/tests.py
+++ ny_viz/tests.py
@@ -112,7 +112,7 @@ class QuestionDetailViewTests(TestCase):
         Detail view of a question with a pub_date in the future returns a 404 not found
         """
         future_question = create_question(question_text="Future question.", days=5)
-        url = reverse("polls:detail", args=(future_question.id,))
+        url = reverse("app_nys:detail", args=(future_question.id,))
         response = self.client.get(url)
         self.assertEqual(response.status_code, 404)
 
@@ -122,6 +122,6 @@ class QuestionDetailViewTests(TestCase):
         displays the question's text.
         """
         past_question = create_question(question_text="Past Question.", days=-5)
-        url = reverse("polls:detail", args=(past_question.id,))
+        url = reverse("app_nys:detail", args=(past_question.id,))
         response = self.client.get(url)
         self.assertContains(response, past_question.question_text)
