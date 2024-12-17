3680025a9705bad2ae69ba2ed2b7253d0fe8abe0
Michael Wheeler <michael.wheeler@compassblue.ai>
2024-12-16 20:30:57 -0500

    Fixed python package name

--- nys_viz_01/views.py
+++ nys_viz_01/views.py
@@ -11,7 +11,7 @@ def index(request):
 
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    return render(request, "nys/detail.html", {"question": question})
+    return render(request, "nys_viz_01/detail.html", {"question": question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
