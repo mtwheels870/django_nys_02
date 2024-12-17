118589d73ae4550e832266a885bcf0a00432f631
Michael Wheeler <michael.wheeler@compassblue.ai>
2024-12-16 20:06:41 -0500

    Playing with views, urls

--- nys_viz_01/urls.py
+++ nys_viz_01/urls.py
@@ -3,5 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # ex: /nys/
     path("", views.index, name="index"),
+    # ex: /nys/5/
+    path("<int:question_id>/", views.detail, name="detail"),
+    # ex: /nys/5/results/
+    path("<int:question_id>/results/", views.results, name="results"),
+    # ex: /nys/5/vote/
+    path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
