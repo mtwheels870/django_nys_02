9fb8dca418e2539728d144bf2f9d0cdc889539f6
Michael Wheeler <michael.wheeler@compassblue.ai>
2024-12-17 10:59:42 -0500

    Fixed response text

--- ny_viz/tests.py
+++ ny_viz/tests.py
@@ -56,7 +56,7 @@ class QuestionIndexViewTests(TestCase):
         """
         response = self.client.get(reverse("app_nys:index"))
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "No (nys) are available.")
+        self.assertContains(response, "No counties are available.")
         self.assertQuerySetEqual(response.context["latest_question_list"], [])
 
     def test_past_question(self):
@@ -78,7 +78,7 @@ class QuestionIndexViewTests(TestCase):
         """
         create_question(question_text="Future question.", days=30)
         response = self.client.get(reverse("app_nys:index"))
-        self.assertContains(response, "No polls are available.")
+        self.assertContains(response, "No counties are available.")
         self.assertQuerySetEqual(response.context["latest_question_list"], [])
 
     def test_future_question_and_past_question(self):
