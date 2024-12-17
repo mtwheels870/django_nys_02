87e532a9633be4fe51eca8778d7ac1aeade4b379
Michael Wheeler <michael.wheeler@compassblue.ai>
2024-12-17 17:10:36 -0500

    Fixing admin screen

--- ny_viz/admin.py
+++ ny_viz/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Question, Choice
+
+class ChoiceInLine(admin.StackedInLine):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date Information", {"fields": ["pub_date"], 
+            "classes": ["collapse"]}),
+    ]
+    inlines = [ChoiceInLine]
 
 # Register your models here.
-admin.site.register(Question)
+admin.site.register(Question, QuestionAdmin)
