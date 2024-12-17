571da76eeb2adb2d7d2a26a862fbf13203ad29ac
Michael Wheeler <michael.wheeler@compassblue.ai>
2024-12-16 21:30:00 -0500

    Fixed typo

--- nys_viz_01/views.py
+++ nys_viz_01/views.py
@@ -23,7 +23,7 @@ def vote(request, question_id):
         # Fix the hard-coded name below (/polls/nys/)
         return render(
             request,
-            "nys/detail.html",
+            "nys/details.html",
             {
                 "question": question,
                 "error_message": "You didn't select a choice.",
