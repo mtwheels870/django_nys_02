a9c263e98e9ebd3ba8947a4d0aca5217e7558f0f
Michael Wheeler <michael.wheeler@compassblue.ai>
2024-12-18 16:41:18 -0500

    1st cut at Markers

--- markers/admin.py
+++ markers/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.contrib.gis import admin
+
+from markers.models import Marker
+
+@admin.register(Marker)
+class MarkerAdmin(admin.GISModelAdmin):
+    list_display = ("name", "location")
 
 # Register your models here.
