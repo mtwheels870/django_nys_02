705378cfadb43ec392d1d1b3f8d97120a78c7452
Michael Wheeler <michael.wheeler@compassblue.ai>
2024-12-18 14:40:42 -0500

    Fixing parens bug

--- markers/urls.py
+++ markers/urls.py
@@ -2,7 +2,5 @@ from django.urls import path
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path(
-        "viewer/", views.TemplateView.as_view(), name="map.html"),
-    ),
+    path("viewer/", views.TemplateView.as_view(), name="map.html"),
 ]
