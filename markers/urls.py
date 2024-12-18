cc030dcb15370879535fd0ae4c67aa57483e4396
Michael Wheeler <michael.wheeler@compassblue.ai>
2024-12-18 14:41:40 -0500

    Still working urls.py

--- markers/urls.py
+++ markers/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
+from . import views
+
 urlpatterns = [
     path("viewer/", views.TemplateView.as_view(), name="map.html"),
 ]
