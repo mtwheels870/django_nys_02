7a151f20d8939eef169eafbf52b06a39433ee49a
Michael Wheeler <michael.wheeler@compassblue.ai>
2024-12-19 16:13:53 -0500

    Fixed admin

--- world/admin.py
+++ world/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from .models import WorldBorder
+
+class WorldBorderAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["name", "iso3"]}),
+        ("Mike's Label Here", {"fields": ["area", "pop2005", "region", "lon", "lat"], 
+            "classes": ["collapse"]}),
+    ]
+
 # Register your models here.
+admin.site.register(WorldBorder, WorldBorderAdmin)
