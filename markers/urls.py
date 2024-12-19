b259aa81143771b0745f03d0704265047e3f9f7b
Michael Wheeler <michael.wheeler@compassblue.ai>
2024-12-18 19:47:28 -0500

    More api

--- markers/urls.py
+++ markers/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path, include
 from django.views.generic import TemplateView
 
-from . import views
+from . import views, api
 
 urlpatterns = [
     path("viewer/", TemplateView.as_view(template_name="markers/map.html")),
-    path("api/", include("./api")),
+    path("api/", api),
 ]
