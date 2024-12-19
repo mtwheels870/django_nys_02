8f1cd464aeaa91ba2836342937a4918ef4570769
Michael Wheeler <michael.wheeler@compassblue.ai>
2024-12-18 22:10:18 -0500

    More nys shit

--- tutorial/apps.py
+++ tutorial/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 class NyVizConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     # What is the name for?
-    name = 'ny_viz'
+    name = 'tutorial'
