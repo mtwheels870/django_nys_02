7c24ad19d94884a9ee1022f0814c9e5fb527738e
Michael Wheeler <michael.wheeler@compassblue.ai>
2024-12-20 10:18:24 -0500

    Working CensusTractModels

--- centralny/load.py
+++ centralny/load.py
@@ -7,7 +7,7 @@ from .models import CensusBorderCounty
     # STATE
     # COUNTY_1
     # STATE_1
-shp_model_mapping = {
+county_mapping = {
     "county_name": "COUNTY",
     "state_name": "STATE",
     "county_code": "COUNTY_1",
@@ -16,9 +16,25 @@ shp_model_mapping = {
     "mpoly": "MULTIPOLYGON",
 }
 
-path_name = "/home/bitnami/Data/County/NY_Counties_04.shp"
-county_shp = Path(path_name)
+COUNTY_PATH = "/home/bitnami/Data/County/NY_Counties_04.shp"
+def run_county(verbose=True):
+    county_shp = Path(COUNTY_PATH)
+    lm = LayerMapping(CensusBorderCounty, county_shp, county_mapping, transform=False)
+    lm.save(strict=True, verbose=verbose)
 
-def run(verbose=True):
-    lm = LayerMapping(CensusBorderCounty, county_shp, shp_model_mapping, transform=False)
+tract_mapping = {
+    "state_code": "STATEFP",
+    "county_code": "COUNTYFP",
+    "tract_id": "TRACTCE",
+    "short_name": "NAME",
+    "long_name": "NAMELSAD",
+    "interp_lat": "INTPTLAT",
+    "interp_long": "INTPTLON",
+    "mpoly": "MULTIPOLYGON",
+}
+
+TRACT_PATH = "/home/bitnami/Data/County/NY_Counties_04.shp"
+def run_county(verbose=True):
+    county_shp = Path(COUNTY_PATH)
+    lm = LayerMapping(CensusBorderCounty, county_shp, county_mapping, transform=False)
     lm.save(strict=True, verbose=verbose)
--- centralny/models.py
+++ centralny/models.py
@@ -18,10 +18,29 @@ class CensusBorderCounty(models.Model):
     # STATE
     state_name = models.CharField(max_length=66)
     # COUNTY_1
-    county_code = models.CharField(max_length=3)
+    county_code = models.CharField(max_length=3, primary_key=True)
     # STATE_1
     state_code = models.CharField(max_length=2)
     pop2000 = models.IntegerField("Population 2000")
+
+    # GeoDjango-specific: a geometry field (MultiPolygonField)
+    mpoly = models.MultiPolygonField()
+
+    # Returns the string representation of the model.
+    def __str__(self):
+        return self.county_name
+
+class CensusTract(models.Model):
+    # Regular Django fields corresponding to the attributes in the
+    # world borders shapefile.
+    state_code = models.CharField(max_length=2)
+    county_code = models.CharField(max_length=3)
+    county = models.ForeignKey(CensusBorderCounty, on_delete=models.CASCADE)
+    tract_id = models.CharField(max_length=6)
+    short_name = models.CharField(max_length=7)
+    long_name = models.CharField(max_length=20)
+    interp_lat = models.CharField(max_length=11)
+    interp_long = models.CharField(max_length=12)
 
     # GeoDjango-specific: a geometry field (MultiPolygonField)
     mpoly = models.MultiPolygonField()
