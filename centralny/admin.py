dd73c38b0226cd269422f4b938620d5efcc64728
Michael Wheeler <michael.wheeler@compassblue.ai>
2024-12-22 10:02:21 -0500

    Admin for other centralny tables

--- centralny/admin.py
+++ centralny/admin.py
@@ -1,10 +1,20 @@
 from django.contrib import admin
 from django.contrib.gis import admin
 
-from centralny.models import Marker
+from centralny.models import Marker, CensusBorderCounty, CensusTract, DeIpRange
 
 @admin.register(Marker)
 class MarkerAdmin(admin.GISModelAdmin):
     list_display = ("name", "location")
 
-# Register your models here.
+@admin.register(CensusBorderCounty)
+class CensusBorderCountyAdmin(admin.GISModelAdmin):
+    list_display = ("name", "location")
+
+@admin.register(CensusTract)
+class CensusTractAdmin(admin.GISModelAdmin):
+    list_display = ("name", "location")
+
+@admin.register(DeIpRange)
+class DeIpRangeAdmin(admin.GISModelAdmin):
+    list_display = ("name", "location")
