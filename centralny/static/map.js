e159da59fd85ef9dcb01e2ee1a89ce6c97e5f16f
Michael Wheeler <michael.wheeler@compassblue.ai>
2024-12-23 10:16:20 -0500

    Use bracket notation

--- centralny/static/map.js
+++ centralny/static/map.js
@@ -33,7 +33,7 @@ async function render_target(url_component, popup_field, myStyle) {
   L.geoJSON(markers, { style: myStyle })
     .bindPopup(
       (layer) =>
-        layer.feature.properties.$popup_field
+        layer.feature.properties[popup_field]
     )
     .addTo(layerGroup);
 }
@@ -49,66 +49,7 @@ async function render_all() {
   } else {
     render_target('ip_ranges', 'de_company_name', {"color": "#b030b0"})
   } 
-  // render_target('markers', 'name', {"color": "#ff7800"})
-  /* render_target('tracts', 'short_name', {"color": "#506030"})
-  render_target('counties', 'county_name', {"color": "#20bb80"}) */
 }
 
 map.on("moveend", render_all)
 
-// map.fitBounds(-75.8, 42.9, -76.2, 43.3);
-// var drawnItems = new L.featureGroup()
-// drawnItems.addLayer(polygon)
-// const layerControl = L.control.layers(drawItems).addTo(map);
-
-// …
-/*
-async function load_markers() {
-  const markers_url = `/centralny/api/markers/?in_bbox=${map
-    .getBounds()
-    .toBBoxString()}`;
-  // console.log("map.js:load_markers, url: " + markers_url)
-  const response = await fetch(
-    markers_url
-  );
-  const geojson = await response.json();
-  return geojson;
-}
-
-async function render_markers() {
-  // console.log("map.js:render_markers")
-  const markers = await load_markers();
-  // Clears our layer group
-  L.geoJSON(markers)
-    .bindPopup(
-      (layer) =>
-        layer.feature.properties.name
-    )
-    .addTo(layerGroup);
-}
-
-async function load_tracts() {
-  // const markers_url = `/centralny/api/markers/?in_bbox=${map
-  const markers_url = `/centralny/api/tracts/?in_bbox=${map
-    .getBounds()
-    .toBBoxString()}`;
-  // console.log("map.js:load_markers, url: " + markers_url)
-  const response = await fetch(
-    markers_url
-  );
-  const geojson = await response.json();
-  return geojson;
-}
-
-async function render_tracts() {
-  // console.log("map.js:render_markers")
-  const markers = await load_tracts();
-  // Clears our layer group
-  L.geoJSON(markers)
-    .bindPopup(
-      (layer) =>
-        layer.feature.properties.short_name
-    )
-    .addTo(layerGroup);
-}
-*/
