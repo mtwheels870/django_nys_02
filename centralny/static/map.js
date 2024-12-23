b6875229a596d5fb894df52730331dd6c35a6956
Michael Wheeler <michael.wheeler@compassblue.ai>
2024-12-22 19:00:54 -0500

    Went back to map.html

--- centralny/static/map.js
+++ centralny/static/map.js
@@ -2,11 +2,18 @@ const osm = "https://www.openstreetmap.org/copyright";
 const copy = `© <a href='${osm}'>OpenStreetMap</a>`;
 const url = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
 const layer = L.tileLayer(url, { attribution: copy });
+/* Create the map layer */
 const map = L.map("map", { layers: [layer] });
 // map.fitWorld();
 const initial_position = [43, -76.2];
-map.setView(initial_position, 10)
+const initial_zoom = 10
+map.setView(initial_position, initial_zoom)
 const layerGroup = L.layerGroup().addTo(map);
+var polygon = L.polygon([
+    [43.1, -75.9],
+    [42.9, -76.1],
+    [43.0, -76.0]]).addTo(map);
+
 // …
 async function load_markers() {
   const markers_url = `/centralny/api/markers/?in_bbox=${map
