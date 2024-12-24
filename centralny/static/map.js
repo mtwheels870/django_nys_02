9d903b39d30a4cf48c2e1b0d5f246bc45445f82c
Michael Wheeler <michael.wheeler@compassblue.ai>
2024-12-24 15:12:09 -0500

    Trying to add leaflet-control-orderlayers

--- centralny/static/map.js
+++ centralny/static/map.js
@@ -4,14 +4,17 @@ const url = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
 
 /* Create the attribution layer, is this added to the layer group 
    Ah, we use it below in the render_markers() */
-const layer = L.tileLayer(url, { attribution: copy });
-const map = L.map("map", { layers: [layer] });
+const layer_attribution = L.tileLayer(url, { attribution: copy });
+const map = L.map("map", { layers: [layer_attribution] });
 // map.fitWorld();
 const initial_position = [43.05, -76.1];
 const initial_zoom = 12.5
 map.setView(initial_position, initial_zoom)
 /* Creates the Layer group */
 const layerGroup = L.layerGroup().addTo(map);
+// const controls = L.control.orderlayers(map)
+var baseLayers = { }
+
 
 async function load_target(url_field) {
   // const markers_url = `/centralny/api/markers/?in_bbox=${map
@@ -30,11 +33,11 @@ async function render_target(url_component, description, popup_field, myStyle) {
   // console.log("map.js:render_target(), popup_field: " + popup_field)
   const targets = await load_target(url_component);
   // Clears our layer group
-  L.geoJSON(targets, { style: myStyle })
+  return L.geoJSON(targets, { style: myStyle })
     .bindPopup(
       (layer) => description + ": <b>" + layer.feature.properties[popup_field] + "</b>"
     )
-    .addTo(layerGroup);
+    // .addTo(layerGroup);
 }
 
 async function render_circle(url_component, description, popup_field, myStyle) {
@@ -42,11 +45,11 @@ async function render_circle(url_component, description, popup_field, myStyle) {
   // console.log("map.js:render_target(), popup_field: " + popup_field)
   const targets = await load_target(url_component);
   // Clears our layer group
-  layer_circle = L.geoJSON(targets, {
+  return L.geoJSON(targets, {
       pointToLayer: function(feature, latLong) {
         return new L.CircleMarker(latLong, myStyle);
       }
-    }).addTo(layerGroup);
+    })
   // We always the circles to be selectable before the polygons
   // layer_circle.bringToFront()
 }
@@ -54,16 +57,19 @@ async function render_circle(url_component, description, popup_field, myStyle) {
 async function render_all() {
   zoom = map.getZoom()
   console.log("render_all(), zoom level: " + zoom)
-  layerGroup.clearLayers();
+  var overlayLayers = {}
+  // layerGroup.clearLayers();
+				var overlayLayers = {
   if (zoom <= 10) {
     style = {
       "color": "#20bb80",
       "fillOpacity": 0.25,
       "weight": 3
     }
-    render_target('counties', 'County Name', 'county_name', style)
+    layer_counties = render_target('counties', 'County Name', 'county_name', style)
+    overlayLayers = {'Counties' : layer_counties }
   } else {
-      if (zoom >= 14) {
+      if (zoom >= 16) {
         // Show the actual IP ranges
         var geojsonMarkerOptions = {
             radius: 5,
@@ -73,9 +79,9 @@ async function render_all() {
             opacity: 1,
             fillOpacity: 0.5
         };
-        render_circle('ip_ranges', 'IP Range: ', 'ip_range_start', geojsonMarkerOptions)
-      }
-      if (zoom <= 15) {
+        layer_ip_ranges = render_circle('ip_ranges', 'IP Range: ', 'ip_range_start', geojsonMarkerOptions)
+        overlayLayers = {'Actual IP Ranges' : layer_ip_ranges }
+      } else {
         circle_style = {
           "color": "#506030",
           "fillOpacity": 0.25,
@@ -83,7 +89,7 @@ async function render_all() {
           "radius": 5,
           "zIndex": 300,
         }
-        render_circle('tract_counts', 'IP ranges in Tract: ', 'range_count', circle_style)
+        layer_centroids = render_circle('tract_counts', 'IP ranges in Tract: ', 'range_count', circle_style)
         // Tracts + their counts
         style = {
           "color": "#506030",
@@ -91,12 +97,17 @@ async function render_all() {
           "weight": 2,
           "zIndex": 400,
         }
-        render_target('tracts', 'Tract Id: ', 'short_name', style)
-        panes = map.getPanes()
+        layer_tracts = render_target('tracts', 'Tract Id: ', 'short_name', style)
+        overlayLayers = {
+            'IP Range Count': layer_centroids,
+            'Tracts': layer_controls
+        };
+        /* panes = map.getPanes()
         var pane_index = 0
         var num_panes = panes.length
-        console.log("draw_tracts(), num_panes = " + num_panes)
+        console.log("draw_tracts(), num_panes = " + num_panes) */
       }
+      var controls = L.control.orderlayers(baseLayers, overlayLayers, {collapsed: false});
   } 
 }
 
