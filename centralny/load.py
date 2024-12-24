04ec802c27f0c7fb7d9405e7cb986fb31109c7e7
Michael Wheeler <michael.wheeler@compassblue.ai>
2024-12-23 20:07:21 -0500

    Fixed multipoint

--- centralny/load.py
+++ centralny/load.py
@@ -95,7 +95,7 @@ class Loader():
         print(f"_c_t_c(), creating new, {census_tract}")
         tract_count = CountRangeTract()
         tract_count.census_tract = census_tract
-        tract_count.mpoint = MultiPointField(Point(census_tract.interp_long, 
+        tract_count.mpoint = MultiPoint(Point(census_tract.interp_long, 
             census_tract.interp_lat))
         self.hash_tracts[census_tract.tract_id] = tract_count
         return tract_count
