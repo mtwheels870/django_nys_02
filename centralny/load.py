592207e3946f6a63d663a7e05f101c8f063b042c
Michael Wheeler <michael.wheeler@compassblue.ai>
2024-12-24 09:39:28 -0500

    5

--- centralny/load.py
+++ centralny/load.py
@@ -129,10 +129,14 @@ class Loader():
             # 2. Tried: centroid(first_polygon), NameError: name 'centroid' is not defined. Did you mean: 'Centroid'?
             # 3.  TypeError: Cannot set CountRangeCounty SpatialProxy (POINT) with value of
             #    type: <class 'django.contrib.gis.db.models.functions.Centroid'>
+            # 4. Take the first mpoly[0]
+            #        first_centroid = first_polygon.centroid()
+            # _create_county_count(), type <class 'django.contrib.gis.geos.polygon.Polygon'>
+            #      TypeError: 'Point' object is not callable, why is this Point?
             first_polygon = county.mpoly[0]
             type_polygon = type(first_polygon)
             print(f"_create_county_count(), type {type_polygon}")
-            first_centroid = first_polygon.centroid()
+            first_centroid = first_polygon.centroid
             #first_centroid = Centroid(first_polygon).output_field
             print(f"_create_county_count(), centroid, {first_centroid}")
             county_counter.centroid = first_centroid
