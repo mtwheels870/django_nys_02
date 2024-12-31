40a8ebed489b798a57da882b4af6c5325c6dc47f
Michael Wheeler <michael.wheeler@compassblue.ai>
2024-12-30 20:46:09 -0500

    updating the database

--- django_nys_02/settings.py
+++ django_nys_02/settings.py
@@ -115,7 +115,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+# WAS TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/New York'
 
 USE_I18N = True
 
--- scheduler-sample/project_sample/settings.py
+++ scheduler-sample/project_sample/settings.py
@@ -12,13 +12,12 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': os.path.join(PROJECT_PATH, 'project_sample.db'),
-        # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'compassblue01',
+        'USER': 'cb_admin',
+        'PASSWORD': 'Ch0c0late!',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
