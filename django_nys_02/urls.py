9b5adaf7f08ce900eb74cbdd3032b64d9106979f
Michael Wheeler <michael.wheeler@compassblue.ai>
2024-12-30 23:50:40 -0500

    Playing w/ URLS

--- django_nys_02/urls.py
+++ django_nys_02/urls.py
@@ -20,8 +20,8 @@ from django.urls import path, include
 # I don't understand why: path("map/api/", can't me moved to the markers/ level, but include("api") won't work
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("tut/", include("tutorial.urls")),
     path("centralny/api/", include("centralny.api")),
     path("centralny/", include("centralny.urls")),
+    path("mycal/", include("mycalendar.urls")),
 ]
 #print(f"project.urlpatterns = {urlpatterns}")
