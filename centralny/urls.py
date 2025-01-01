49ed561e75b35d7da1e4453ae091687e6477c086
Michael Wheeler <michael.wheeler@compassblue.ai>
2024-12-31 23:18:19 -0500

    fixed urls

--- centralny/urls.py
+++ centralny/urls.py
@@ -7,10 +7,10 @@ from . import views, api
 # path("ping/", TemplateView.as_view(template_name="centralny/ping_strategy.html")),
 urlpatterns = [
     path("map/", TemplateView.as_view(template_name="centralny/map.html")),
-    path("ping/", views.IndexView.as_view(), name="index"),
+    path("ping/", views.PingStrategyIndexView.as_view(), name="index"),
     # ex: /tutorial/5/
-    path("ping/<int:pk>/", views.DetailView.as_view(), name="detail"),
+    path("ping/<int:pk>/", views.PingStrategyDetailView.as_view(), name="detail"),
     # ex: /tutorial/5/results/
-    path("ping/<int:pk>/results/", views.ResultsView.as_view(), name="results"),
+    path("ping/<int:pk>/results/", views.PingStrategyResultsView.as_view(), name="results"),
 ]
 #print(f"markers.urlpatterns = {urlpatterns}")
