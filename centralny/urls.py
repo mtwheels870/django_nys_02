21c19a0b5491ba76e440ac25bbb8bd8d57bca4df
Michael Wheeler <michael.wheeler@compassblue.ai>
2025-01-01 20:18:21 -0500

    Changed url to use pk

--- centralny/urls.py
+++ centralny/urls.py
@@ -13,10 +13,10 @@ urlpatterns = [
     path("map/", TemplateView.as_view(template_name="centralny/map.html")),
     path("ping/", views.PingStrategyIndexView.as_view(), name="ping_strat_index"),
     # ex: /tutorial/5/
-    path("ping/<int:id>/", views.PingStrategyDetailView.as_view(), name="ping_strat_detail"),
+    path("ping/<int:pk>/", views.PingStrategyDetailView.as_view(), name="ping_strat_detail"),
     # ex: /tutorial/5/results/
     path("ping/<int:pk>/results/", views.PingStrategyResultsView.as_view(), name="ping_strat_results"),
     # ex: /tutorial/5/vote/
-    path("ping/<int:id>/configure", views.configure_ping, name="configure_ping"),
+    path("ping/<int:pk>/configure", views.configure_ping, name="configure_ping"),
 ]
 #print(f"markers.urlpatterns = {urlpatterns}")
