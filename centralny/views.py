ab016518eabfe40a3e1bd4f64b06ba551b7db0e8
Michael Wheeler <michael.wheeler@compassblue.ai>
2025-01-02 08:46:55 -0500

    import

--- centralny/views.py
+++ centralny/views.py
@@ -2,6 +2,7 @@ import logging
 from django.shortcuts import render
 from django.utils import timezone
 from django.views import generic
+from django.http import HttpResponse, HttpResponseRedirect
 
 from rest_framework import viewsets
 from rest_framework_gis import filters
