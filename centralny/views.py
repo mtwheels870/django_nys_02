82c31acf4e4bfc2cc59ea1b6f09d938d44276534
Michael Wheeler <michael.wheeler@compassblue.ai>
2025-01-02 08:47:45 -0500

    reverse

--- centralny/views.py
+++ centralny/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.utils import timezone
 from django.views import generic
 from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
 from rest_framework import viewsets
 from rest_framework_gis import filters
