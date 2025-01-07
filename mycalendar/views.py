d0c72971d59e01af9bb47acc3c6e01b8daf21152
Michael Wheeler <michael.wheeler@compassblue.ai>
2025-01-07 16:54:52 -0500

    Redirect to a different page

--- mycalendar/views.py
+++ mycalendar/views.py
@@ -46,13 +46,13 @@ def set_schedule_type(request, pk):
         # Fix the hard-coded name below (/polls/nys/)
         return render(
             request,
-            "./schedsurv_create.html",
+            "./schedsurv_done.html",
             {
-                "survey_id": pk,
                 "error_message": "You didn't select a schedule_type.",
             },
         )
     else:
+        new_id = 23
         # What does F() do?
         #selected_choice.votes = F("votes") + 1
         #selected_choice.save()
@@ -62,4 +62,4 @@ def set_schedule_type(request, pk):
         # This works (back to the same thing... not what we want):
         # reverse(r"app_my_scheduler:schedule_survey_detail", args=(pk,)))
         # Can't use reverse(), that's only with named stuff (app_name)
-        return HttpResponseRedirect(f"schedule/calendar/daily/{PP_CALENDAR_SLUG}")
+        return HttpResponseRedirect(reverse("app_my_scheduler:done", args(pk=new_id,)))
