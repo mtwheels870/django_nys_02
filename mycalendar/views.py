8f0db991a8be12581671dfec07f142d83e7fada2
Michael Wheeler <michael.wheeler@compassblue.ai>
2025-01-06 21:17:01 -0500

    Trying to get types working

--- mycalendar/views.py
+++ mycalendar/views.py
@@ -24,3 +24,10 @@ class ScheduleSurveyDetailView(generic.DetailView):
     model = IpRangeSurvey
     template_name = "./schedsurv_create.html"
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['types'] = [1, 2, 3]
+        # pk = self.kwargs.get('pk')  # Or 'product_id' if you customized the parameter name
+        # Use pk to access the object or do other operations
+        # print(f"PingStrategyDetailView.get_context_data(), pk = {pk}")
+        return context
