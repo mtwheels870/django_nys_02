2786c7d07d872e754b9edd02b2c88ed6e370d637
Michael Wheeler <michael.wheeler@compassblue.ai>
2025-01-07 20:54:46 -0500

    Did all of the calendar init in the app config

--- mycalendar/apps.py
+++ mycalendar/apps.py
@@ -1,8 +1,9 @@
-from django.apps import AppConfig
+import sys
 
+from django.apps import AppConfig
 from django.core.signals import request_finished
 
-PINPOINT_CAL_SLUG = "pp"
+CALENDAR_SLUG_PP = "pp"
 
 # I think this name becomes the leading prefix on the database table names, etc.
 #    def __init__(self): self.calendar_pp = Calendar.objects.filter(slug__eq=PINPOINT_CAL_SLUG)
@@ -11,5 +12,17 @@ class MyCalendarConfig(AppConfig):
     name = 'mycalendar'
 
     def ready(self):
-        from . import signals
-        request_finished.connect(signals.my_callback)
+        # from . import signals
+        # request_finished.connect(signals.my_callback)
+        print("MyCalendarConfig.ready(), checking for existing data ...")
+        try:
+            self.pp_calendar = Calendar.objects.get(slug=CALENDAR_SLUG_PP)
+            self.rule_daily = Rule.objects.get(name="Daily")
+        except Calendar.DoesNotExist:
+            print(f"Could not find calendar(slug) {CALENDAR_SLUG_PP}")
+            sys.exit(1)
+        except Rule.DoesNotExist:
+            print("Could not find calendar rules")
+            sys.exit(1)
+        self.today = datetime.date.today()
+        print("Finished configuring calendar")
--- mycalendar/views.py
+++ mycalendar/views.py
@@ -10,7 +10,7 @@ import django.dispatch
 from rest_framework import viewsets
 from rest_framework_gis import filters
 
-PP_CALENDAR_SLUG = "pp"
+from schedule.models import (Calendar, Event, Rule)
 
 from centralny.models import (
     IpRangePing,
@@ -52,7 +52,10 @@ def set_schedule_type(request, pk):
             },
         )
     else:
-        our_calendar = Calendar.objects.get(slug=PP_CALENDAR_SLUG)
+        app_name = self.request.resolver_match.app_name
+        app_config = apps.get_app_config(app_name)
+        today = app_config.today
+        print(f"set_schedule_type(), today: {today}")
         scheduled_survey = ScheduledIpRangeSurvey(calendar=our_calendar,
             ip_range_survey=survey, time_approved=timezone.now(),
             survey_type=selected_sched_type)
