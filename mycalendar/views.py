00f719f7d6483b95e317e31c431cac383cac89b0
Michael Wheeler <michael.wheeler@compassblue.ai>
2025-01-08 13:00:51 -0500

    Moved position arg

--- mycalendar/views.py
+++ mycalendar/views.py
@@ -66,7 +66,7 @@ def set_schedule_type(request, pk):
             ip_range_survey=survey, time_approved=timezone.now(),
             survey_type=sched_type)
         scheduled_survey.save()
-        call_command('fire_zmap', verbosity=3, interactive=False, pk)
+        call_command('fire_zmap', pk, verbosity=3, interactive=False)
         new_id = scheduled_survey.id
         return HttpResponseRedirect(reverse("app_my_scheduler:dailycalendar", args=(new_id,)))
 
