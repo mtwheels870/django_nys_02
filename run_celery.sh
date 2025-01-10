30563177a23d238cd09c48e652c87e8c9dbe4c00
Michael Wheeler <michael.wheeler@compassblue.ai>
2025-01-10 09:51:07 -0500

    created run_celery

---
+++ run_celery.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+LOG_DIR=/tmp/pp_celery
+LOG_FILE=celery_"$(date +'%FT%H%m').txt"
+FULL_FILE=$LOG_DIR/$LOG_FILE
+
+RUN_DIR=./django_nys_02
+LOG_LEVEL=INFO
+APP_NAME=tasks
+
+echo cd $RUN_DIR
+cd $RUN_DIR
+celery -A $APP_NAME worker --loglevel=$LOG_LEVEL | tee $FULL_FILE
