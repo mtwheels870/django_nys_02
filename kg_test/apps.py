ec33c4a77f9c5e61f6099b841b068254aacad27b
Michael Wheeler <michael.wheeler@compassblue.ai>
2025-01-17 14:30:48 +0000

    /kb/kg/

---
+++ kg_test/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class KgTestConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'kg_test'
---
+++ kg_train/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class KgTrainConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'kg_train'
