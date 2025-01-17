36c1c90fdeeb8cb6acb6db60ef8cb5966ce9dc94
Michael Wheeler <michael.wheeler@compassblue.ai>
2025-01-17 12:24:40 -0500

    more views

--- kg_admin/urls.py
+++ kg_admin/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from django.views.generic import TemplateView
 
+from . import views
+
 # path("api/markers", views.MarkerViewSet.as_view({'get': 'list'}), name="markers")
 # path("ping/", TemplateView.as_view(template_name="centralny/ping_strategy.html")),
 
