5e793e1b090c661ff4bdd8f19a41001df4b5bf98
Michael Wheeler <michael.wheeler@compassblue.ai>
2025-01-17 20:29:00 -0500

    import

--- kg_admin/views.py
+++ kg_admin/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views import generic
 
-from templatetags.navigation import KgApp
+from .templatetags.navigation import KgApp
 #class KgApp:
 #    def __init__(self, name, url):
 #        self.name = name;
