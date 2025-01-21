7cadd15e6062306c3baec32fa0601b8451a3bd07
Michael Wheeler <michael.wheeler@compassblue.ai>
2025-01-21 18:10:42 -0500

    FormView

--- kg_train/views.py
+++ kg_train/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
+from django.views.generic.edit import FormView
+
 from django.utils import timezone
 
 from .models import TextFileStatus, TextFile
