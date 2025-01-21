f2a09eb39e89deff0145f5c419d001160b76201e
Michael Wheeler <michael.wheeler@compassblue.ai>
2025-01-21 17:53:59 -0500

    Stuff

--- kg_train/urls.py
+++ kg_train/urls.py
@@ -7,7 +7,9 @@ urlpatterns = [
     # ex: /tutorial/.  Note, these are all formal classes (IndexView, DetailView...) inside views.py
     path("", views.IndexView.as_view(), name="index"),
 
-    path("upload/", views.upload_file, name="upload_file"),
+    path("kgtrain/upload/", "kg_train/upload_form.html", name="upload_form"),
+
+    path("kgtrain/upload_submit/", views.upload_file, name="upload_file"),
     # ex: /tutorial/5/
 ]
 #    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
