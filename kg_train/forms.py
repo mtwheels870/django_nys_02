d4f4ff2f3e3dbf4b78606a82d8b34a41a3b60d20
Michael Wheeler <michael.wheeler@compassblue.ai>
2025-01-21 17:29:19 -0500

    Try and set the initial status (from the form)

--- kg_train/forms.py
+++ kg_train/forms.py
@@ -5,4 +5,10 @@ from .models import TextFile
 class UploadFileForm(forms.ModelForm):
     class Meta:
         model = TextFile
-        fields = [ 'file_name', 'file', 'status' ]
+        fields = [ 'file', 'file_name', 'status' ]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if 'status' in self.data:
+            self.fields['status'].initial = 1
+
