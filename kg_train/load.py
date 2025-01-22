a56332c89292e10075ea2cf9698f0dcaea3bfdda
Michael Wheeler <michael.wheeler@compassblue.ai>
2025-01-22 13:27:41 -0500

    Creating labels

--- kg_train/load.py
+++ kg_train/load.py
@@ -8,7 +8,24 @@ from django.contrib.gis.db.models.functions import Centroid
 
 from .models import (
     TextFileStatus, 
-    TextFile)
+    TextFile,
+    NerLabel)
+
+label_dictionary = {
+    "PERSON":"People, including fictional.",
+    "NORP" : "Nationalities or religious or political groups.",
+    "FAC" : "Buildings, airports, highways, bridges, etc.",
+    "ORG" : "Companies, agencies, institutions, etc.",
+    "GPE" : "Countries, cities, states.",
+    "EVENT" : "Named hurricanes, battles, wars, sports events, etc.",
+    "DATE": "Absolute or relative dates or periods.",
+    "TIME": "Times smaller than a day.",
+    "QUANTITY": "Measurements, as of weight or distance.",
+    "ORDINAL", "first, second, etc.",
+    "CARDINAL", "one, two, three...",
+    "MILITARY", "Military forces, such as PLAN",
+    "CIVILIAN", "Civil forces, such as maritime militia, fishing fleets",
+}
 
 class Loader():
     def __init__(self):
@@ -19,3 +36,9 @@ class Loader():
         for single_status in status:
             ts = TextFileStatus(description=single_status)
             ts.save()
+
+    def create_labels(self, verbose=True):
+        for _, (key, value) in enumerate(label_dictionary):
+            print(f"create_labels(), {key} = {value}")
+            ner_label = NerLabel(short_name=key, description=value)
+            ner_label.save()
--- kg_train/models.py
+++ kg_train/models.py
@@ -8,8 +8,16 @@ class TextFileStatus(models.Model):
         return f"{self.id}: {self.description}"
 
 class TextFile(models.Model):
-    file_name = models.CharField(max_length=80)
+    file_name = models.CharField("Name (windows / AWS)", max_length=80)
     file = models.FileField(upload_to="uploads/")
+    file_size = models.IntegerField("File Size (bytes)", null=True)
+    time_uploaded = models.DateTimeField(null=True)
     status = models.ForeignKey(TextFileStatus, on_delete=models.CASCADE)
     def __str__(self):
         return f"{self.file_name}, status = {self.status}"
+
+class NerLabel(models.Model):
+    short_name = models.CharField("Short", max_length=10)
+    description = models.CharField("Long", max_length=80)
+    def __str__(self):
+        return self.short_name
