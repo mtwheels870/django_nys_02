54e6f9a54ee6253047d6bad3cc80fb2a4b8ce29f
Michael Wheeler <michael.wheeler@compassblue.ai>
2025-01-22 15:31:48 -0500

    file_name, size

--- kg_train/views.py
+++ kg_train/views.py
@@ -37,11 +37,9 @@ def upload_file(request):
             # This uses the Form to create an instance (TextFile)
             text_file = form.save()
             file = form.cleaned_data['file']
-            for key,value in enumerate(file.items()):
-                print(f"file[{key}] = {value}")
-            file_name = file.file_name
-            file_size = file.file_size
-            print(f"upload_file(), (cleaned) file_name = {file_name}, file_size ={file_size}")
+            text_file.file_name = file.name
+            text_file.file_size = file.size
+            print(f"upload_file(), (cleaned) file_name = {file.name}, file_size ={file.size}")
             # Should overwrite file_name here
             text_file.save()
             return HttpResponseRedirect(reverse("app_kg_train:index"))
