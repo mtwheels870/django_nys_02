ef3db5b8fb9d5a39d36d9757400aef78cabb4b54
Michael Wheeler <michael.wheeler@compassblue.ai>
2025-01-23 15:22:22 -0500

    More folder stuff

--- kg_train/forms.py
+++ kg_train/forms.py
@@ -4,11 +4,5 @@ from .models import DocumentSet
 
 class UploadFolderForm(forms.ModelForm):
     class Meta:
-        model = TextFile
-        fields = [ 'file', 'file_name', 'status' ]
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        if 'status' in self.data:
-            self.fields['status'].initial = 1
-
+        model = DocumentSet
+        fields = [ 'file' ]
