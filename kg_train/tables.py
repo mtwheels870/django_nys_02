4a286bdfda2c994a4c86e15afa9cef55e2d399ff
Michael Wheeler <michael.wheeler@compassblue.ai>
2025-01-24 12:05:00 -0500

    checkbox

--- kg_train/tables.py
+++ kg_train/tables.py
@@ -3,7 +3,8 @@ import django_tables2 as tables
 from .models import TextFile
 
 class TextFileTable(tables.Table):
+    selection = tables.CheckBoxColumn(accessor="pk")
     class Meta:
         model = TextFile
         template_name = "django_tables2/bootstrap-responsive.html"
-        fields = ["page_number", "file_name", "file_size", "status", "body"]
+        fields = ["selection", "page_number", "file_name", "file_size", "status", "body"]
