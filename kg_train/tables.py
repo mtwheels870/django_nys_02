9a8ed23edbfc41df614a4009facba3635907f391
Michael Wheeler <michael.wheeler@compassblue.ai>
2025-01-24 09:12:21 -0500

    Added tables.py

---
+++ kg_train/tables.py
@@ -0,0 +1,9 @@
+import django_tables2 as tables
+
+from .models import TextFile
+
+class TextFileTable(tables.Table):
+    class Meta:
+        model = TextFile
+        template_name = "django_tables2/bootstrap.html"
+        fields = ["page_number", "file_name", "file_size", "status", "body"]
