d4adaadf8bec4984f3169d02e6c29916cea4e7f9
Michael Wheeler <michael.wheeler@compassblue.ai>
2025-01-24 17:23:44 -0500

    Back to text (on both)

--- kg_train/models.py
+++ kg_train/models.py
@@ -44,7 +44,7 @@ class TextFile(models.Model):
     file_size = models.IntegerField("File Size (bytes)", null=True)
     status = models.ForeignKey(TextFileStatus, on_delete=models.CASCADE)
     # body = models.OneToOneField(Document, on_delete=models.CASCADE, null=True)
-    text = ProseEditorField("stuff01", preset="announcements")
+    text = ProseEditorField("text", preset="announcements")
 
     def __str__(self):
         return self.display_name()
