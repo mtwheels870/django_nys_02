eb66c6fd5f3c1e7ad0ced1d920e0967b2af12796
Michael Wheeler <michael.wheeler@compassblue.ai>
2025-01-27 08:42:24 -0500

    Dump out file contents

--- kg_train/tasks.py
+++ kg_train/tasks.py
@@ -8,21 +8,33 @@ from celery import shared_task
 from .models import TextFileStatus, TextFile, TextFolder
 
 VENV_PYTHON_PATH="/home/bitnami/nlp/venv01/bin"
+FILE_TEXT = "text_file.txt"
 
-def make_tmp_files(file_id):
+def make_tmp_files():
     temp_directory = "/tmp/invoke_prodigy"
     now = datetime.datetime.now()
     folder_snapshot = now.strftime("%Y%m%d_%H%M%S")
     full_path = os.path.join(temp_directory, folder_snapshot)
     if not os.path.exists(full_path):
-        os.makedirs(temp_directory)
-    print("tasks.py:make_tmp_files(), directory = {temp_directory}")
+        os.makedirs(full_path)
+    print(f"tasks.py:make_tmp_files(), full_path = {full_path}")
+    return full_path
+
+def generate_prodigy_files(dir_path, file_id):
+    text_file = TextFile.objects.filter(id=file_id)
+    file_path_text = os.path.join(dir_path, FILE_TEXT)
+    with open(file_path_text, "w") as file_writer:
+        file_content = text_file.prose_editor 
+        file_writer.write(file_content)
+    print(f"tasks.py:generate_prodigy_files(), file_path_text = {file_path_text}")
+    return file_path_text 
 
 
 @shared_task
 def invoke_prodigy(x, y, folder_id, file_id):
     print(f"tasks.py:add(), adding {x} and {y}, file_id = {file_id}")
-    make_tmp_files(file_id)
+    dir_path = make_tmp_files(file_id)
+    file_path_text = generate_prodigy_files(dir_path, file_id)
 
     return x + y
 
