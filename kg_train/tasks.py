a2384efd5ff0bcce9839886629a1266110bb2de2
Michael Wheeler <michael.wheeler@compassblue.ai>
2025-01-27 15:07:20 -0500

    fixing args

--- kg_train/tasks.py
+++ kg_train/tasks.py
@@ -58,8 +58,7 @@ def invoke_prodigy(self, x, y, folder_id, file_id):
     dir_path = make_tmp_files()
     file_path_text = generate_prodigy_files(dir_path, file_id)
     result = x + y
-    signals.task_postrun.send(self, self.task_id, self, args=[], kwargs={"key":"value"}, 
-                 retval=status.SUCCESS)
+    signals.task_postrun.send(self, self.id, self, retval=status.SUCCESS, args=[], kwargs={"key":"value"}) 
     # signals.task_success.send(sender=self, result=result)
     return result
 
