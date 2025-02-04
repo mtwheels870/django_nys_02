edf50918794829886dd02d264da1b0811a6e17b0
Michael Wheeler <michael.wheeler@compassblue.ai>
2025-02-03 22:19:28 -0500

    Put other Prodigy models in

--- kg_train/models_prodigy.py
+++ kg_train/models_prodigy.py
@@ -10,3 +10,22 @@ class PrdgyDataset(models.Model):
         db_table = 'dataset'  # Optional: specify the table name if different
         managed = False  # Important: prevent Django from managing migrations for this table
         app_label = 'prodigy'  # Specify the app label for this model
+
+class PrdgyExample(models.Model):
+    input_hash = models.BigIntegerField()
+    content = models.BinaryField()
+    task_hash = models.BigIntegerField()
+
+    class Meta:
+        db_table = 'example'  # Optional: specify the table name if different
+        managed = False  # Important: prevent Django from managing migrations for this table
+        app_label = 'prodigy'  # Specify the app label for this model
+
+class PrdgyLink(models.Model):
+    example_id = models.ForeignKey(PrdgyExample, on_delete=models.CASCADE)
+    dataset_id = models.ForeignKey(PrdgyDataset, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'link'  # Optional: specify the table name if different
+        managed = False  # Important: prevent Django from managing migrations for this table
+        app_label = 'prodigy'  # Specify the app label for this model
