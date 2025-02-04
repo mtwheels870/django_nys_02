c33e3741661f7b85ed29166d06a51178f631c2ba
Michael Wheeler <michael.wheeler@compassblue.ai>
2025-02-04 12:57:58 -0500

    render_file_name

--- kg_train/tables.py
+++ kg_train/tables.py
@@ -9,7 +9,8 @@ class TextFileTable(tables.Table):
         template_name = "django_tables2/bootstrap-responsive.html"
         # template_name = "django_tables2/bootstrap.html"
         fields = ["selection", "page_number", "file_name", "status", "time_edited", "time_labeled", "file_size"]
-    class render_file_name(self, value, record):
+
+    def render_file_name(self, value, record):
         return format_html("<b>{}</b>", value)
 
 class NerLabelTable(tables.Table):
