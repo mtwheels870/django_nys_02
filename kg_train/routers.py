aa29e950f2b0834cf6e8c6b99abdffb2da38055b
Michael Wheeler <michael.wheeler@compassblue.ai>
2025-02-03 21:54:18 -0500

    routers.py

--- kg_train/routers.py
+++ kg_train/routers.py
@@ -1,6 +1,6 @@
 class SecondaryRouter:
     def db_for_read(self, model, **hints):
-        if model._meta.app_label == 'prodigy'
+        if model._meta.app_label == 'prodigy':
             print(f"SecondaryRouter.db_for_read(), app_label = {model._meta.app_label}, model = {model.__name__}")
             return 'secondary'
         return 'default'
