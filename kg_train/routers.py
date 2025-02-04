88c8a6de88301f3fbdebb059bd4c9a000a51589a
Michael Wheeler <michael.wheeler@compassblue.ai>
2025-02-03 20:30:32 -0500

    Print model name

--- kg_train/routers.py
+++ kg_train/routers.py
@@ -1,6 +1,6 @@
 class SecondaryRouter:
     def db_for_read(self, model, **hints):
-        # print(f"SecondaryRouter.db_for_read(), app_label = {model._meta.app_label}")
+        print(f"SecondaryRouter.db_for_read(), app_label = {model._meta.app_label}, model = {model.__name__}")
         if model._meta.app_label == 'my_secondary_app':
             return 'secondary'
         return 'default'
