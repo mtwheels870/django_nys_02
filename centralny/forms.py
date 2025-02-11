1f533e10de5ca8f2c32dff9d543626f86b7f68f7
Michael Wheeler <michael.wheeler@compassblue.ai>
2025-02-10 20:42:47 -0500

    plain form

--- centralny/forms.py
+++ centralny/forms.py
@@ -2,6 +2,6 @@ from django import forms
 
 from .models import CountRangeTract
 
-class SelectedCensusTractForm(forms.ModelForm):
+class SelectedCensusTractForm(forms.Form):
     id = forms.IntegerField(widget=forms.HiddenInput())
 
