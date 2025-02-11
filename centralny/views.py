ae17b4ef3cecb228c872951bbd336bcd2b007ba4
Michael Wheeler <michael.wheeler@compassblue.ai>
2025-02-11 18:10:28 -0500

    stuff

--- centralny/views.py
+++ centralny/views.py
@@ -127,6 +127,8 @@ class MapNavigationView(generic.edit.FormView):
         return context_data
 
     def post(self, request, *args, **kwargs):
+        in_bbox = request.POST.get('in_bbox')
+        print(f"in_bbox = {in_bbox}")
         form = SelectedCensusTractForm(request.POST)
         if form.is_valid():
             id = form.cleaned_data['id']
