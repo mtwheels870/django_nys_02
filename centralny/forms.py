d4b46bea57e60bf66338dcde2c794c665c8dd505
Michael Wheeler <michael.wheeler@compassblue.ai>
2025-02-10 20:41:33 -0500

    stuff

--- centralny/forms.py
+++ centralny/forms.py
@@ -1,8 +1,7 @@
 from django import forms
-from django_prose_editor.fields import ProseEditorFormField
 
 from .models import CountRangeTract
 
 class SelectedCensusTractForm(forms.ModelForm):
-    id = forms.IntField(widget=forms.HiddenInput())
+    id = forms.IntegerField(widget=forms.HiddenInput())
 
