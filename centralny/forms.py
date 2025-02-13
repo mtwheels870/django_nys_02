40bee048fb2b82c053eb6aad551997c87032a971
Michael Wheeler <michael.wheeler@compassblue.ai>
2025-02-13 13:32:02 -0500

    Show the forms, but readonly

--- centralny/forms.py
+++ centralny/forms.py
@@ -2,7 +2,14 @@ from django import forms
 
 from .models import CountRangeTract
 
+# widget=forms.HiddenInput())
 class SelectedCensusTractForm(forms.Form):
-    agg_type = forms.CharField(widget=forms.HiddenInput())
-    id = forms.IntegerField(widget=forms.HiddenInput())
-    map_bbox = forms.CharField(widget=forms.HiddenInput())
+    agg_type = forms.CharField()
+    id = forms.IntegerField()
+    map_bbox = forms.CharField()
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['agg_type'].widget.attrs['readonly'] = True
+        self.fields['id'].widget.attrs['readonly'] = True
+        self.fields['map_bbox'].widget.attrs['readonly'] = True
