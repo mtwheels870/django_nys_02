51f7c6d5530e9b83f17426176143a1a3dfb35439
Michael Wheeler <michael.wheeler@compassblue.ai>
2025-02-24 15:54:29 -0500

    stuff

--- centralny/tasks.py
+++ centralny/tasks.py
@@ -27,7 +27,6 @@ TOTAL_OBJECTS = 2000
 
 #@shared_task(bind=True)
 #def start_range_survey(self, *args, **kwargs):
-@shared_task(bind=True)
 class RangeSurvey(Task):
     def __init__(self):
         self.batches = []
@@ -59,6 +58,7 @@ class RangeSurvey(Task):
                 break
         return outer_loop, index_range 
 
+    @shared_task(bind=True)
     def run(self, *args, **kwargs):
         print(f"start_range_survey(), self = {self}, kwargs = {kwargs}, creating survey")
 
