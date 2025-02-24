88e213f9c121c48e9990b80f6501796cd65534ad
Michael Wheeler <michael.wheeler@compassblue.ai>
2025-02-23 20:44:48 -0500

    stuff

--- centralny/views.py
+++ centralny/views.py
@@ -199,6 +199,7 @@ class ConfigurePingView(generic.edit.FormView):
         # After this, the form is created
 
         form = context_data['form']
+        my_field = context_data['my_field']
         print(f"CPV.g_c_d(), kwargs = {kwargs}, form = {form}, my_field = {my_field}")
 # Put some celery stuff here
         # File stuff
