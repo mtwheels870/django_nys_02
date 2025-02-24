ed150e2c5336ee7b7c6dfcfa0e49b35d55d5a9cc
Michael Wheeler <michael.wheeler@compassblue.ai>
2025-02-24 16:01:14 -0500

    return batches

--- centralny/tasks.py
+++ centralny/tasks.py
@@ -24,39 +24,40 @@ SMALL_CHUNK_SIZE = 10000
 TOTAL_OBJECTS = 2000
 
 #@shared_task(bind=True)
+# Nested method
+def _get_all_ranges(survey, tract, index_range):
+    batches = []
+    outer_loop = True
+    get_range_chunks = True
+    range_start = 0
+    range_end = range_start + SMALL_CHUNK_SIZE
+    print(f"get_all_ranges(), tract_id = {tract.id}")
+    while get_range_chunks:
+        print(f"get_all_ranges(), getting {SMALL_CHUNK_SIZE} ranges, getting[{range_start},{range_end}]")
+        ip_ranges = tract.deiprange_set.all().order_by("id")[range_start:range_end]
+        ranges_returned = ip_ranges.count()
+        for range in ip_ranges:
+            if index_range % 200 == 0:
+                print(f"start_range_survey(), creating range[{index_range:05}], {range.ip_range_start}")
+            range_ping = IpRangePing(ip_survey=survey,ip_range=range)
+            range_ping.save()
+            index_range = index_range + 1
+            if index_range > TOTAL_OBJECTS:
+                print(f"start_range_survey(), index_range > 10000, breaking")
+                outer_loop = False
+                get_range_chunks = False
+                break
+        range_start = range_end
+        range_end = range_end + SMALL_CHUNK_SIZE
+        if ranges_returned < SMALL_CHUNK_SIZE:
+            break
+    return outer_loop, index_range, batches
+
 #def start_range_survey(self, *args, **kwargs):
 @shared_task(bind=True)
 def start_range_survey(self, *args, **kwargs):
-    # Nested method
-    def _get_all_ranges(self, survey, tract, index_range):
-        outer_loop = True
-        get_range_chunks = True
-        range_start = 0
-        range_end = range_start + SMALL_CHUNK_SIZE
-        print(f"get_all_ranges(), tract_id = {tract.id}")
-        while get_range_chunks:
-            print(f"get_all_ranges(), getting {SMALL_CHUNK_SIZE} ranges, getting[{range_start},{range_end}]")
-            ip_ranges = tract.deiprange_set.all().order_by("id")[range_start:range_end]
-            ranges_returned = ip_ranges.count()
-            for range in ip_ranges:
-                if index_range % 200 == 0:
-                    print(f"start_range_survey(), creating range[{index_range:05}], {range.ip_range_start}")
-                range_ping = IpRangePing(ip_survey=survey,ip_range=range)
-                range_ping.save()
-                index_range = index_range + 1
-                if index_range > TOTAL_OBJECTS:
-                    print(f"start_range_survey(), index_range > 10000, breaking")
-                    outer_loop = False
-                    get_range_chunks = False
-                    break
-            range_start = range_end
-            range_end = range_end + SMALL_CHUNK_SIZE
-            if ranges_returned < SMALL_CHUNK_SIZE:
-                break
-        return outer_loop, index_range 
 
     # Main method
-    self._batches = []
     print(f"start_range_survey(), self = {self}, kwargs = {kwargs}, creating survey")
 
     survey = IpRangeSurvey()
@@ -70,7 +71,7 @@ def start_range_survey(self, *args, **kwargs):
     for tract_count in count_range_tracts:
         tract = tract_count.census_tract
         try:
-            outer_loop, index_range = self._get_all_ranges(survey, tract, index_range)
+            outer_loop, index_range, batches = self._get_all_ranges(survey, tract, index_range)
         # Get all of the ranges from this census tract
         except (KeyError, DeIpRange.DoesNotExist):
             print(f"start_range_survey(), Exception, no ranges for tract = {tract.id}")
