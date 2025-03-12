31630a0fdb0f740f6fa698ba133a61d9587940cf
Michael Wheeler <michael.wheeler@compassblue.ai>
2025-03-12 10:07:02 -0400

    abc

--- powerscan/views.py
+++ powerscan/views.py
@@ -332,7 +332,7 @@ class ConfigurePingView(generic.edit.FormView):
         # Load up the celery details for the next form
         celery_details = self._get_celery_details()
         field_survey_id = form.fields['field_survey_id']
-        field_survey_id.initial = _self.survey_id
+        field_survey_id.initial = self._survey_id
         # FIELD_SURVEY_ID : self._survey_id}
         context = {"form" : form,
             FIELD_CELERY_DETAILS : celery_details,
