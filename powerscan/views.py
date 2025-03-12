8d5f949bfa5e13ee9ad2bf5ed99fa4b2d1462638
Michael Wheeler <michael.wheeler@compassblue.ai>
2025-03-12 11:03:46 -0400

    abc

--- powerscan/views.py
+++ powerscan/views.py
@@ -335,6 +335,8 @@ class ConfigurePingView(generic.edit.FormView):
         # field_survey_id.data = 23
         print(f"CPV.post(), reloading page, survey_id = {self._survey_id}, field = {field_survey_id}")
         print(f"     dir(field) = {dir(field_survey_id)}")
+        widget = field_survey_id.widget
+        print(f"     dir(widget) = {dir(widget)}")
         # No Work: field_survey_id.initial = self._survey_id
         # field_survey_id = self._survey_id
         # FIELD_SURVEY_ID : self._survey_id}
