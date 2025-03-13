199f8626d5a6c06d69ec8cf07573ead9f8b13386
Michael Wheeler <michael.wheeler@compassblue.ai>
2025-03-13 17:01:20 -0400

    took out debugging

--- powerscan/ping.py
+++ powerscan/ping.py
@@ -283,7 +283,6 @@ class PingSurveyManager:
             saddr = row['saddr']
             timestamp = row['timestamp-ts']
             self._writer_cidr_trie.write(f"Trie_lookup: {saddr}\n")
-            print(f"_match_zmap_replies(), saddr = {saddr}")
             # results = self.trie.find_all(saddr)
             results = self.trie_wrapper.find_all(saddr)
             num_results = len(results)
