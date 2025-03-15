3795cb5f676cb4ed64c35c3264583f41ec159f92
Michael Wheeler <michael.wheeler@compassblue.ai>
2025-03-15 17:19:02 -0400

    abc

--- powerscan/views_ping.py
+++ powerscan/views_ping.py
@@ -109,6 +109,7 @@ class ConfigurePingView(generic.edit.FormView):
             kwargs={"survey_id" : survey_id},
             queue=QUEUE_NAME,
             routing_key='ping.tasks.build_whitelist')
+        celery_results_handler.save_pending(async_result)
         return async_result
 
 #    @task_postrun.connect(sender=build_whitelist)
