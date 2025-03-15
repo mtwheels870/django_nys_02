06f811c86878902fd03e165dcde7eaa24618a8a2
Michael Wheeler <michael.wheeler@compassblue.ai>
2025-03-14 20:52:35 -0400

    configure ws

--- django_nys_02/asgi.py
+++ django_nys_02/asgi.py
@@ -9,8 +9,11 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 
 import os
 
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_nys_02.settings')
 
@@ -18,6 +21,12 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_nys_02.settings')
 django_asgi_app = get_asgi_application()
 print(f"django_asgi_app = {django_asgi_app}")
 
+# Our URLs are in here
+from powerscan.routing import websocket_urlpatterns
+
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
+    "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
+    ),
 })
