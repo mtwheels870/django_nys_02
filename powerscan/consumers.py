fd9fc92420b166275e932b94050fc768bccce253
Michael Wheeler <michael.wheeler@compassblue.ai>
2025-03-15 21:51:05 -0400

    abc

--- powerscan/consumers.py
+++ powerscan/consumers.py
@@ -2,7 +2,7 @@ import json
 
 from enum import Enum
 
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import WebsocketConsumer, AsyncWebsocketConsumer
 
 class CeleryResultsHandler:
     # State machines for ping stuff
@@ -47,19 +47,43 @@ class CeleryResultsHandler:
 
 celery_results_handler = CeleryResultsHandler()
 
-class TaskConsumer(WebsocketConsumer):
+class TaskConsumer(AsyncWebsocketConsumer):
     groups = ["task_updates"]
 
-    def connect(self):
-        self.accept()
+    async def connect(self):
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = f"chat_{self.room_name}"
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
 
-    def disconnect(self, close_code):
-        pass
+        await self.accept()
 
-    def task_completed(self, event):
-        message = event["message"]
-        print(f"TaskConsumer.task_completed(), message = {message}")
-        self.send(text_data=json.dumps({"message" : message}))
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
+
+    async def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+    
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'chat.message',
+                'message': message
+            }
+        )
+    
+        async def chat_message(self, event):
+            message = event['message']
+    
+            await self.send(text_data=json.dumps({
+                'message': message
+            }))
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
