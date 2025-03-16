ffae42eecbccf828f5853db67ad84656e54a2533
Michael Wheeler <michael.wheeler@compassblue.ai>
2025-03-16 11:35:12 -0400

    stuff

--- powerscan/load.py
+++ powerscan/load.py
@@ -327,8 +327,9 @@ class Loader():
         data = 123
         print(f"Loader.ping_channel(), pinging")
         channel_layer = get_channel_layer()
+        channel_name = "test-worker"
         print(f"ping_channel(), channel_layer = {channel_layer}, channel_name = {channel_name}")
-        async_to_sync(channel_layer.send)("test_worker", {
+        async_to_sync(channel_layer.send)(channel_name, {
             "type": "triggerWorker",
         })
         #channel_name = "task-one"
