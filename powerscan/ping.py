3a468d4ff06f19bc9a5e5e5dcea3dc162bf30d3a
Michael Wheeler <michael.wheeler@compassblue.ai>
2025-03-23 14:57:38 -0400

    abc

--- powerscan/ping.py
+++ powerscan/ping.py
@@ -305,7 +305,7 @@ class PingSurveyManager:
 
     def _match_zmap_replies(self):
         index_chunk = 0
-        for chunk in read_csv(self.path_output, chunksize=PD_CHUNK_SIZE)
+        for chunk in read_csv(self.path_output, chunksize=PD_CHUNK_SIZE):
             column_names = chunk.columns.tolist()
             print(f"_match_zmap_replies(), chunk[{index_chunk}], rows = {df.shape[0]}, column = {column_names}")
             #print(f"_match_zmap_replies(), self = {self}, trie = {self.pyt}")
