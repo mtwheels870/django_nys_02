fbf972262a0c4c6ef02047f80c4e8a8875cf1601
Michael Wheeler <michael.wheeler@compassblue.ai>
2025-03-24 20:40:09 -0400

    abc

--- powerscan/forms.py
+++ powerscan/forms.py
@@ -2,6 +2,10 @@ from django import forms
 from django.forms import ModelForm
 from django.forms.widgets import SplitDateTimeWidget
 
+#from django.forms import DateTimeField
+from django.core.exceptions import ValidationError
+from django.utils.dateparse import parse_datetime
+
 from .models import CountTract, IpRangePing, UsState
 
 # widget=forms.HiddenInput())
@@ -33,10 +37,23 @@ class PingStrategyForm(forms.Form):
     # field_survey_id = forms.CharField(widget=forms.TextInput(attrs={'readonly': 'readonly'}),
     field_survey_id = forms.CharField(label="Survey Id (if created)", initial="0")
 
+class CustomDateTimeField(DateTimeField):
+    def to_python(self, value):
+        print(f"CFTF.to_python(), value = {value}")
+        if isinstance(value, list):
+            if all(v is None for v in value):
+                return None
+            value = ' '.join(str(v) for v in value if v is not None)
+        try:
+            return parse_datetime(value)
+        except (ValueError, TypeError):
+            raise ValidationError("Enter a valid date/time.")
+
 class ScheduleSurveyForm(forms.Form):
     field_survey_id = forms.IntegerField(label="Survey ID:")
     field_survey_name = forms.CharField(label="States:")
-    field_start_time = forms.DateTimeField(label="Start Time (first ping):", widget=SplitDateTimeWidget)
+    field_start_time = forms.CustomDateTimeField(label="Start Time (first ping):", widget=SplitDateTimeWidget)
+    # field_start_time = forms.DateTimeField(label="Start Time (first ping):", widget=SplitDateTimeWidget)
     field_recurring = forms.DurationField(label="Recurring (amount)", required=False)
 
     def __init__(self, *args, **kwargs):
