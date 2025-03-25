f3246e8e36b939d9867fc9263a64d445c9497a95
Michael Wheeler <michael.wheeler@compassblue.ai>
2025-03-25 16:17:08 -0400

    abc

--- powerscan/tasks_periodic.py
+++ powerscan/tasks_periodic.py
@@ -16,8 +16,8 @@ from django_nys_02.celery import app as celery_app, QUEUE_NAME
 
 @celery_app.task(name='blah_de_blah', bind=True)
 class SurveyScheduler(Task):
-    def __init__(self, args, kwargs):
-        print(f"SS.__init__(), args = {args}, kwargs = {kwargs}")
+    def __init__(self):
+        print(f"SS.__init__()")
         self.stuff = "Yellow"
 
     def run(self, arg1, arg2):
