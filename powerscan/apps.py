e550a279ee19f87e9dc03e9b25c96715c9e81006
Michael Wheeler <michael.wheeler@compassblue.ai>
2025-03-25 15:06:04 -0400

    abc

--- powerscan/apps.py
+++ powerscan/apps.py
@@ -4,7 +4,7 @@ from django_nys_02.celery import app as celery_app, QUEUE_NAME
 
 from .tasks_periodic import periodic_task_to_do
 
-@celery_app.task
+@celery_app.task(name='blah_de_blah')
 def blah_de_blah(arg1, arg2):
     print(f"blah_de_blah(), arg1 = {arg1}, arg2 = {arg2}")
 
