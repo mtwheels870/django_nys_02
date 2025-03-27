3079f4fcd4b89a2b448e5718451b8e3b46e48899
Michael Wheeler <michael.wheeler@compassblue.ai>
2025-03-26 20:23:31 -0400

    abc

--- powerscan/tasks_periodic.py
+++ powerscan/tasks_periodic.py
@@ -32,7 +32,7 @@ def start_ping(survey_id, delay_secs):
 
     delay_mins, delay_secs = _estimate_zmap_time(survey_id)
 
-    async_result = zmap_from_file.apply_async((countdown=delay_secs,
+    async_result = zmap_from_file.apply_async(countdown=delay_secs,
             kwargs={"survey_id" : survey_id},
                 #"ip_source_id": IP_RANGE_SOURCE },
             queue=QUEUE_NAME,
@@ -66,6 +66,7 @@ def _estimate_zmap_time(survey_id):
     #print(first + second)
     return estimated_mins, estimated_secs
 
+@task
 def _start_tally(metadata_file, survey_id, delay_mins, delay_secs):
     from .tasks import tally_results
 
