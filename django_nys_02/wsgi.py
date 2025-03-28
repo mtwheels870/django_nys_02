f812bebf38363720bbd64bbc0535267e5e65745b
Michael Wheeler <michael.wheeler@compassblue.ai>
2025-03-28 16:45:35 -0400

    put the DJANGO_SETTINGS thing back in

--- django_nys_02/wsgi.py
+++ django_nys_02/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_nys_02.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_nys_02.settings')
 
 application = get_wsgi_application()
