5d5804be014c67502df3f2f9b4e571ca2bfe79e9
Michael Wheeler <michael.wheeler@compassblue.ai>
2025-04-01 21:21:55 -0400

    abc

--- django_nys_02/settings.py
+++ django_nys_02/settings.py
@@ -208,7 +208,7 @@ LOGGING = {
     'loggers': {
         '': {
             'handlers': ['console'],
-            'level': 'DEBUG',
+            'level': 'INFO',
             'propagate': True,
         },
         'django.request': {
