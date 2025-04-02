ebf1b0cf7dc444981bb2fc91a2e818d68ca8a104
Michael Wheeler <michael.wheeler@compassblue.ai>
2025-04-02 16:46:58 -0400

    added get() method

--- powerscan/views.py
+++ powerscan/views.py
@@ -138,6 +138,10 @@ class MapNavigationView(generic.edit.FormView):
         context_data['table'] = table
         return context_data
 
+    def get(self, request, *args, **kwargs):
+        print(f"MNV.get(), request = {request}")
+        return self.render_to_response(self.get_context_data()
+
     def post(self, request, *args, **kwargs):
         form = SelectedCensusTractForm(request.POST)
         # print(f"MNV.post(), checking form here: {form}")
