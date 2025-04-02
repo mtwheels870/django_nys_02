96fc1bd3bb9bd168ac154af4bfeed1abca5a478e
Michael Wheeler <michael.wheeler@compassblue.ai>
2025-04-02 09:09:36 -0400

    abc

--- powerscan/views_ping.py
+++ powerscan/views_ping.py
@@ -204,8 +204,8 @@ class RecentSurveyView(SingleTableView):
 
     def _calculate_map_extent(self, survey_id):
         bbox = SurveyUtil.calculate_bbox(survey_id)
-        print(f"_calculate_map_extent(), logger = {logger}")
-        print(f"_calculate_map_extent(), survey_id = {survey_id}, bbox = {bbox}")
+        print(f"(PRINT) _calculate_map_extent(), logger = {logger}")
+        logger.info(f"(PRINT) _calculate_map_extent(), survey_id = {survey_id}, bbox = {bbox}")
         base_url = reverse("app_powerscan:map_viewer")
         query_params = {'q': 'django', 'page' : 2}
         url = f"{base_url}?{urlencode(query_params)}"
