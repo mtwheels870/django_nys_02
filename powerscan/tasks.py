8f5ebb5caf2c41956905f12c15dda8e862396fe9
Michael Wheeler <michael.wheeler@compassblue.ai>
2025-04-30 15:33:08 -0400

    Channels

--- powerscan/tasks.py
+++ powerscan/tasks.py
@@ -42,8 +42,8 @@ from django.utils import timezone
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 
-from channels.layers import get_channel_layer
-from asgiref.sync import async_to_sync
+#from channels.layers import get_channel_layer
+# from asgiref.sync import async_to_sync
 
 # Our stuff
 from django_nys_02.settings import CELERY_QUEUE
