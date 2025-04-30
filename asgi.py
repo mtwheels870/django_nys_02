d12e31d59320dd111f37e918e170099ea3bf0479
Michael Wheeler <michael.wheeler@compassblue.ai>
2025-04-30 19:06:11 -0400

    Put default ASGI back in

---
+++ asgi.py
@@ -0,0 +1,37 @@
+"""
+ASGI config for DJANGO project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+#from channels.auth import AuthMiddlewareStack
+#from channels.routing import ProtocolTypeRouter, URLRouter, ChannelNameRouter
+#from channels.security.websocket import AllowedHostsOriginValidator
+
+from django.core.asgi import get_asgi_application
+
+from powerscan.consumers import TaskConsumer, CHANNEL_NAME_TASK_RESULT
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_nys_02.settings')
+
+# application = get_asgi_application()
+#django_asgi_app = get_asgi_application()
+#print(f"django_asgi_app = {django_asgi_app}, dir() = {dir(django_asgi_app)}")
+
+# Our URLs are in here
+from powerscan.routing import websocket_urlpatterns
+
+application = get_asgi_application()
+#    "http": django_asgi_app,
+#    "websocket": AllowedHostsOriginValidator(
+#        AuthMiddlewareStack(URLRouter(websocket_urlpatterns))),
+#    'channel': ChannelNameRouter({
+#        'task-one': TaskConsumer.as_asgi(actions={"generate":"task_one"}),
+#        'task-two': TaskConsumer.as_asgi(actions={"delete":"task_two"}),
+#        'test-worker': TestWorker.as_asgi(),
+#        }),
