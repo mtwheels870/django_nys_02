8b1c2b1837c3b0fd8c3b39457963b5f1debe553a
Michael Wheeler <michael.wheeler@compassblue.ai>
2025-06-13 20:17:38 +0000

    Version builds, fails on run (b/c of POSTGRES_PASSWORD)

--- build_psql.sh
+++ build_psql.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 BUILDKIT="DOCKER_BUILDKIT=1"
 DOCKER_BUILD="docker build"
-SECRET="--secret id=psql_password,env=POSTGRES_PASSWORD"
+ROOT_PASSWORD="psql_root_password"
+SECRET_ROOT="--secret id=${ROOT_PASSWORD},env=POSTGRES_PASSWORD"
+USER_PASSWORD="psql_user_password"
+SECRET_USER="--secret id=${USER_PASSWORD},env=POSTGRES_USER_PASSWORD"
 REST="-f Dockerfile.psql -t mtw_psql:01 ."
-CMD="${BUILDKIT} ${DOCKER_BUILD} ${SECRET} ${REST}"
+CMD="${BUILDKIT} ${DOCKER_BUILD} ${SECRET_ROOT} ${SECRET_USER} ${REST}"
 echo $CMD
 eval $CMD
