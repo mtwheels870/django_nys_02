4f2a55dfc9638751fd5721f28b460863bc4399d0
Michael Wheeler <michael.wheeler@compassblue.ai>
2025-08-06 20:05:30 -0400

    hard-code the null

--- powerscan/ping.py
+++ powerscan/ping.py
@@ -378,7 +378,7 @@ class PingSurveyManager:
                 # return_value = cursor.execute(f"CALL create_whitelist({self._survey_id}, null)")
                 string_value = "null"
                 # return_value = cursor.execute("CALL create_whitelist(%s, null)", [self._survey_id])
-                return_value = cursor.execute("CALL create_whitelist", [int(self._survey_id), string_value])
+                return_value = cursor.execute("CALL create_whitelist(%s,null);", [int(self._survey_id)])
                 print(f"return_value = {return_value}")
                 rows = cursor.fetchall()
                 for index, row in enumerate(rows):
