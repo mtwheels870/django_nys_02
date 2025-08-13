4a0049ccba370f76dfe3a334e7ac4b0304cd6d3c
Michael Wheeler <michael.wheeler@compassblue.ai>
2025-08-12 21:32:11 -0400

    Fix saddr_row

--- powerscan/ping.py
+++ powerscan/ping.py
@@ -484,7 +484,7 @@ class PingSurveyManager:
                 saddr_raw = row['saddr-raw']
                 # timestamp = row['timestamp-ts']
                 # self._writer_cidr_trie.write(f"Trie_lookup: {saddr}\n")
-                print(f"match_zmap(), before ip_address(), saddr_raw = {saddr_raw}, type() = {type(saddr_row)}")
+                print(f"match_zmap(), before ip_address(), saddr_raw = {saddr_raw}, type() = {type(saddr_raw)}")
                 ip_address = ipaddress.ip_address(saddr_raw)
                 print(f"match_zmap(), after ip_address()")
                 string_ip = ip_address.str()
